db8436d59f32ff8694f1dbfacbf554bdda8f98a6
John Mickelonis <jmick@email.com>
2022-11-01 11:26:07 -0400

    Added guard to prevent crash when switching scene collections.

--- browser-client.cpp
+++ browser-client.cpp
@@ -680,6 +680,9 @@ bool BrowserClient::OnConsoleMessage(CefRefPtr<CefBrowser>,
 				     const CefString &message,
 				     const CefString &source, int line)
 {
+	if (!valid())
+		return false;
+
 	int errorLevel = LOG_INFO;
 	const char *code = "Info";
 	switch (level) {
