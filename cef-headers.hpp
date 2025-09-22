b673d66d2968d3da93f42c31f308f62770229dbb
John Mickelonis <jmick@email.com>
2025-09-22 08:56:44 -0400

    Integrated changes between 2.25.3 and 2.26.0 (bdabf83 -> c3b1e81).

--- cef-headers.hpp
+++ cef-headers.hpp
@@ -55,12 +55,6 @@
 #include "include/views/cef_window.h"
 #endif
 
-#if CHROME_VERSION_BUILD >= 4430
-#define ENABLE_WASHIDDEN 1
-#else
-#define ENABLE_WASHIDDEN 0
-#endif
-
 #if !defined(_WIN32) && !defined(__APPLE__) && \
 	(CHROME_VERSION_BUILD >= 6943 || (CHROME_VERSION_BUILD > 6337 && defined(CEF_OSR_EXTRA_INFO)))
 #define ENABLE_BROWSER_SHARED_TEXTURE
